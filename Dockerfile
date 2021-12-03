fe8f003f6bc897e45bbb81514c5b84e8703612c2
Paolo Zilioti <pzilioti@gmail.com>
2021-12-02 22:43:03 -0300

    static files with gunicorn

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /usr/src/app
 COPY requirements.txt ./
 RUN pip install -r requirements.txt
 COPY . .
-
+RUN python manage.py migrate
 CMD ["gunicorn", "-b", "0.0.0.0:8000", "visualizacao_cotacao.wsgi"]
   
--- visualizacao_cotacao/wsgi.py
+++ visualizacao_cotacao/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'visualizacao_cotacao.settings')
 
-application = get_wsgi_application()
+
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
